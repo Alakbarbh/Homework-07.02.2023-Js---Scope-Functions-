eaa58926a3052c0ed27ff0f73f574320a7fac19a
Alakbar B. Hasanov <alakbarbh@code.local>
2023-02-07 18:47:34 +0400

    Homework(07.02.2023 Js - Scope, Functions)

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+"use strict"
+
+// 1) Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapan bir function yazin.
+
+// let n =21
+
+// function division (n) {
+
+//     if (n % 3 == 0 && n % 7 == 0) {
+//         console.log("Bolunur");
+//     }else{
+//         console.log("Bolunmur");
+//     }
+// }
+
+// division(n);
+
+
+
+
+// let n = 21
+
+// let division = (n) => {
+//     if (n % 3 == 0 && n % 7 == 0) {
+//         console.log("Bolunur");
+//     } else {
+//         console.log("Bolunmur");
+//     }
+// }
+
+// division(n)
+
+
+
+
+
+
+
+
+
+
+// 2) Functiona musbet bir eded gelir, hemin funksiya gelen ededin faktorialini hesablayib geri qaytarmalidir. 
+
+
+let 
+
+function factorial(n) {
+    
+    for()
+}
+
